14eda497b4baf498ed73ac4a128e55a8d246caa6
Kishore Bhatia <bhatia.kishore@gmail.com>
2021-11-10 20:20:55 +0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     <h1>Hello, {formatName(greet)}!</h1>;
+     <h1>Hello, {formatName(greet)}!</h1>
 	</header>
     </div>
   );
